2c76f41eab9d2f218b7794534ecbb314d79264ad
Sam Hiatt <sam.hiatt@gmail.com>
2015-07-15 23:29:45 -0700

    Added express router.

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 var http = require('http');
-var milight = require('node-milight');
-var ctrl = require('node-milight')({host:'192.168.42.100',delay:100}).WhiteController;
+var milight = require('node-milight')({host:'192.168.42.100',delay:100});
+var app = require('express')(); 
 
-const PORT=8037; 
+const PORT=8037;
 
-function handleRequest(request, response){
-    if (request.url=='/off') {
-        ctrl.off(1);
-        response.end('OK. Lights off.');
-    } else if (request.url=='/on') {
-        ctrl.on(1);
-        response.end('OK. Lights on.');
-    } else {
-        response.end('Command not understood.');
+app.get('/',function(req,res){
+    res.send('Command not understood.');
+});
+
+app.get('/:controller/:group/:command/:val?',function(req,res) {
+    var commandName = req.params.command;
+    var controller = (req.params.controller=='w')? milight.WhiteController 
+      : (req.params.controller=='rgbw')? milight.RGBWController : {};
+    var command = controller[commandName];
+    var group = parseInt(req.params.group) || 0;
+    var val = parseInt(req.params.val) || null;
+    if (typeof command != 'function') res.send('Command not understood.');
+    else {
+        command(group,function(err){
+            if (err) throw err;
+            res.send("OK, lights "+group+" "+commandName+" val: "+val);
+        });
     }
-}
+});
 
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function(){
+app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
